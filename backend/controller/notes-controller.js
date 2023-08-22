a91f113fd71199168c7e5e789d3fc9235a23584c
Arsh <harshtyagi7678@gmail.com>
2023-08-23 00:42:07 +0530

    All running

--- backend/controller/notes-controller.js
+++ backend/controller/notes-controller.js
@@ -1,16 +1,21 @@
-
+import {NotesModel} from '../models/notes-schema.js';
 export const notecontroller = {
-    submitNote(request,response){
-
+    async submitNote(request,response){
+        const noteObject = request.body;
+        try{
+            const doc = await NotesModel.create(noteObject);
+            // console.log("Note Created",body);
+            response.json({message:"Note Created","note":doc});
+        }
+        catch(err){
+            response.json({message:"Error in Note Creation"});
+        }
     },
     updateNote(request,response){
-
     },
     deleteNote(request,response){
-
     },
     getNote(request,response){
-
+        
     }
-
 }
