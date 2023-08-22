279bb4ea8c49dd8d98a987a7e00a939eb60829f2
Arsh <harshtyagi7678@gmail.com>
2023-08-22 23:57:50 +0530

    added controller functions

---
+++ backend/controller/notes-controller.js
@@ -0,0 +1,16 @@
+
+export const notecontroller = {
+    submitNote(request,response){
+
+    },
+    updateNote(request,response){
+
+    },
+    deleteNote(request,response){
+
+    },
+    getNote(request,response){
+
+    }
+
+}
